7aaa7f2e76ad3bc377d082051dd74f23550a2819
Ruvian <ruvian.fabrin@hotmail.com>
2022-08-12 19:36:58 -0300

    Criado a classe emprestimo

---
+++ src/biblioteca/Emprestimo.java
@@ -0,0 +1,21 @@
+package biblioteca;
+
+public class Emprestimo {
+	private Livro livro;
+	private Usuario usuario;
+	
+	public Livro getLivro() {
+		return livro;
+	}
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+
+}
